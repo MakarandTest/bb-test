e7536a6f489abd54017585de13937a503b67ac43
Makarand <makarandtapkir@gmail.com>
2026-08-22 17:53:02 +0530

    Add vulnerable sql code Snyk testing

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 print("Hello from my Snyk test application")
+import sqlite3
+
+def get_user(user_id):
+    conn = sqlite3.connect("users.db")
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return conn.execute(query).fetchall()
